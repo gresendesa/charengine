89f95bfd9691973b557465f0b7b5148458cc0bce
Guilherme Sá <gresendesa@gmail.com>
2022-03-06 17:35:30 -0300

    canvas

--- canvas/script.js
+++ canvas/script.js
@@ -6,11 +6,46 @@ class Cell {
         this.column = column
     }
 
-    draw(){
+    draw(color){
         let ctx = this.grid.context
+
+        ctx.fillStyle = 'green'
+        ctx.strokeStyle = 'yellow'
+        if(this.line % 2 ^ this.column % 2){
+            ctx.fillStyle = 'gray'
+            ctx.strokeStyle = 'red'
+        }
+
+        if(color != undefined) {
+            ctx.fillStyle = color
+            ctx.strokeStyle = color
+            ctx.fillRect(this.grid.cellSize * this.column, this.grid.cellSize * this.line, this.grid.cellSize, this.grid.cellSize)
+        }
+
+        ctx.fillRect(this.grid.cellSize * this.column, this.grid.cellSize * this.line, this.grid.cellSize, this.grid.cellSize)
+
         ctx.beginPath();
-        ctx.rect(this.grid.cellSize * this.column, this.grid.cellSize * this.line , this.grid.cellSize, this.grid.cellSize);
-        ctx.stroke();
+
+        ctx.moveTo(this.grid.cellSize * this.column, this.grid.cellSize * this.line + 0.5)
+        ctx.lineTo(this.grid.cellSize * this.column + this.grid.cellSize, this.grid.cellSize * this.line + 0.5)
+
+        ctx.moveTo(this.grid.cellSize * this.column + this.grid.cellSize - 0.5, this.grid.cellSize * this.line)
+        ctx.lineTo(this.grid.cellSize * this.column + this.grid.cellSize - 0.5, this.grid.cellSize * this.line + this.grid.cellSize)
+        
+        ctx.moveTo(this.grid.cellSize * this.column + this.grid.cellSize, this.grid.cellSize * this.line + this.grid.cellSize - 0.5)
+        ctx.lineTo(this.grid.cellSize * this.column, this.grid.cellSize * this.line + this.grid.cellSize - 0.5)
+        
+        ctx.moveTo(this.grid.cellSize * this.column + 0.5, this.grid.cellSize * this.line + this.grid.cellSize)
+        ctx.lineTo(this.grid.cellSize * this.column + 0.5, this.grid.cellSize * this.line)
+
+        let fontSize = this.grid.cellSize / 1.1
+        ctx.textAlign = "center";
+        ctx.font = `${fontSize}px Consolas`;
+
+        ctx.strokeText("A", this.grid.cellSize * this.column + (this.grid.cellSize / 2), this.grid.cellSize * this.line + (this.grid.cellSize / 2));
+
+        ctx.lineWidth = 1
+        ctx.stroke()
     }
 
 }
@@ -45,18 +80,30 @@ class Grid {
     }
 }
 
+const canvas = document.getElementById('canvas')
+const context = canvas.getContext('2d');
+const grid = new Grid(context)
 
-const render = () => {
+const render = event => {
 
-    const canvas = document.getElementById('canvas')
-    const context = canvas.getContext('2d');
     canvas.width = window.innerWidth
     canvas.height = window.innerHeight
 
-    let grid = new Grid(context)
-
-    let cellSize = canvas.width / 40 //Definir um cellSize para cada tamanho de tela
+    let cellSize = Math.floor(canvas.width / 20) //Definir um cellSize para cada tamanho de tela
     grid.draw(canvas.width, canvas.height, cellSize)
+
+    if(event.type == 'load'){
+        canvas.addEventListener('click', e => {
+            let rect = canvas.getBoundingClientRect()
+            let x = e.clientX - rect.left
+            let y = e.clientY - rect.top
+            let cellLine = Math.floor(y / grid.cellSize)
+            let cellColumn = Math.floor(x / grid.cellSize)
+            console.log(`x ${x} y ${y} line ${cellLine} column ${cellColumn} size ${grid.cellSize}`)
+            let cell = grid.cells[cellLine][cellColumn]
+            cell.draw('blue')
+        }, false)
+    }
 
 }
 
